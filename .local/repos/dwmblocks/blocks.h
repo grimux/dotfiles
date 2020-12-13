475184a1a927921cfc298fb44bde8e0777bd92c7
grimux <grimux@tuta.io>
2020-12-13 12:20:03 -0800

    Adjusted spacing for easier reading

--- .local/repos/dwmblocks/blocks.h
+++ .local/repos/dwmblocks/blocks.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",    "sharedrive",                                                   0,              12},
-	{"",    "forecast",                                                     18000,          5},
-	{"  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"",    "battery",                                                      5,              3},
-	{"  ", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/*Icon*/	/*Command*/		                                        /*Update Interval*/	/*Update Signal*/
+	{"",            "sharedrive",                                                   0,                      12},
+	{"",            "forecast",                                                     18000,                  5},
+	{"  ",         "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	                0},
+	{"",            "battery",                                                      5,                      3},
+	{"📅 ",         "date '+%b %d (%a) %I:%M%p'",					5,	                0},
+	{"",            "internet",                                                     5,                      4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
