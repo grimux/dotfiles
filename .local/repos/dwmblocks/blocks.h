e7c281eec12b9bd000c193fa2819acda79c267fc
grimux <grimux@tuta.io>
2020-12-13 13:44:27 -0600

    Added cpu usage script The script relies on the `mpstat` utility from the `sysstat` package

--- .local/repos/dwmblocks/blocks.h
+++ .local/repos/dwmblocks/blocks.h
@@ -3,13 +3,14 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                                        /*Update Interval*/	/*Update Signal*/
 	{"",            "sharedrive",                                                   0,                      12},
 	{"",            "forecast",                                                     18000,                  5},
-	{"",            "cputemp",                                                      10,                     1},
-	{"  ",         "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	                2},
+	{"🌡",           "sensors | awk '/Core 0/ {print $3}'",                          5,                      4},
+	{"CPU: ",       "mpstat | awk '/all/ {print $4 \"%\"}'",                          5,                      6},
+	{" ",         "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	                2},
 	{"",            "battery",                                                      5,                      3},
-	{"📅 ",         "date '+%b %d (%a) %I:%M%p'",					5,	                0},
+	{"📅",         "date '+%b %d (%a) %I:%M%p'",					5,	                0},
 	{"",            "internet",                                                     5,                      4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
