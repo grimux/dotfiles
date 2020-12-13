7bfefd0e30c76e9989f7d5fdff780b5e5dacc00a
grimux <grimux@tuta.io>
2020-12-13 12:23:44 -0800

    Added `cputemp` script

--- .local/repos/dwmblocks/blocks.h
+++ .local/repos/dwmblocks/blocks.h
@@ -3,7 +3,8 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                                        /*Update Interval*/	/*Update Signal*/
 	{"",            "sharedrive",                                                   0,                      12},
 	{"",            "forecast",                                                     18000,                  5},
-	{"  ",         "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	                0},
+	{"",            "cputemp",                                                      10,                     1},
+	{"  ",         "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	                2},
 	{"",            "battery",                                                      5,                      3},
 	{"📅 ",         "date '+%b %d (%a) %I:%M%p'",					5,	                0},
 	{"",            "internet",                                                     5,                      4},
