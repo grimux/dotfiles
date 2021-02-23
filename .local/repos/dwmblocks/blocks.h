1af676e8e8db947948a8a206f47ef473c5722659
grimux <grimux@tuta.io>
2021-02-23 07:39:25 -0500

    Added volume module to status bar

--- .local/repos/dwmblocks/blocks.h
+++ .local/repos/dwmblocks/blocks.h
@@ -5,12 +5,15 @@ static const Block blocks[] = {
 	{"",            "sharedrive",                                                   0,                      12},
 	{"",            "forecast",                                                     18000,                  5},
 	{"🌡",           "sensors | awk '/Core 0/ {print $3}'",                          5,                      4},
-	{"CPU: ",       "mpstat | awk '/all/ {print $4 \"%\"}'",                          5,                      6},
+	{"CPU: ",       "mpstat | awk '/all/ {print $4 \"%\"}'",                        5,                      6},
 	{" ",         "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	                2},
 	{"",            "battery",                                                      5,                      3},
+	{"",            "volume",                                                       5,                      40},
 	{"📅",         "date '+%b %d (%a) %I:%M%p'",					5,	                0},
 	{"",            "internet",                                                     5,                      4},
 };
+
+// To update any element of the status bar use `pkill -RTMIN+x dwmblocks` where x is the Update Signal from above
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
