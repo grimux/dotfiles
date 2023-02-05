10bf9c7c247f3264a8f7e76b07a2b23b3a2c3f9c
Jacob Meredith <jakerm13@gmail.com>
2023-02-05 09:53:40 -0600

    Expanded the script with some more functions. Added header for errors and debugging.

--- .local/bin/portable/update_playlist.sh
+++ .local/bin/portable/update_playlist.sh
@@ -1,45 +1,130 @@
 #!/usr/bin/bash
-
+#
 # Script to update a YouTube Playlist.
+#
+# e - script stops on error
+# u - error if undefined variable
+# o pipefail - script fails if command piped fails
+# x - output each line (debug)
+#
+set -euox pipefail
+#set -euo pipefail
+
+##############
+### Config ###
+##############
+# Title of playlist.
+playlist_name=""
 
 # URL of the playlist to keep updated.
 playlist_url=""
 
-# Name of playlist file.
-playlist_name=""
-
-# Resolution to get
+# Default resolution to download.
 resolution="1080"
 
-# Formatting for video output
+# Playlist file for UMS and mpv
+m3u_directory="$HOME/Videos/playlists"
+m3u_file="whatever.m3u"
+
+# Archive location and file.  Used to track download history.
+archive_directory="$HOME/.config/youtube-dl/archives"
+archive_file="whatever.archive"
+
+# Ramdomized m3u playlist
+randomized_m3u=0
+
+# Formatting for video file name.
 output_format="%(playlist_index)s - %(title)s.%(ext)s"
+format_settings="bestvideo[height<=$resolution][ext=webm]+bestaudio"
 
-# Playlist file for UMS
-playlist_file="$HOME/Videos/playlists/$playlist_name.m3u"
 
-# Archive file to track downloaded episodes.
-archive_file="./archive.txt"
 
-# Call to yt-dlp
-yt-dlp \
-	--paths "./" \
-	--output "$output_format" \
-	--format "bestvideo[height<=$resolution][ext=webm]+bestaudio" \
-	--embed-subs \
-	--embed-thumbnail \
-	--add-metadata \
-	--download-archive "$archive_file" \
-	--sponsorblock-remove sponsor \
-	--merge-output-format mkv \
-	"$playlist_url"
+#################
+### Functions ###
+#################
+# Function to download the playlist, using yt-dlp.
+download_playlist() {
+	# Call to yt-dlp
+	yt-dlp \
+		--paths "./" \
+		--output "$output_format" \
+		--format "$format_settings" \
+		--embed-subs \
+		--embed-thumbnail \
+		--add-metadata \
+		--download-archive "$archive_directory/$archive_file" \
+		--sponsorblock-remove sponsor \
+		--restrict-filenames \
+		--merge-output-format mkv \
+		"$playlist_url"
+}
+	
+# Create an m3u playlist.
+create_m3u() {
+	local m3u_file_path="$m3u_directory/$m3u_file"
+	echo "Creating playlist for:  \"$(basename "$m3u_file_path")\""
 
-# Remove old playlist if it exists.
-if [ -f "$playlist_file" ]; then
-	rm $playlist_file
-	echo "#EXTM3U" > $playlist_file
-fi
+	# Remove old playlist if it exists.
+	if [ -f "$m3u_file_path" ]; then
+		rm "$m3u_file_path"
+	fi
+	
+	# Create an m3u playlist.
+	for file in ./*.mkv; do
+		readlink --canonicalize "$file" >> "$m3u_file_path"
+	done
 
-# Create a m3u playlist for UMS and mpv
-for file in ./*.mkv; do
-	echo $file >> $playlist_file
-done
+	if [ "$randomized_m3u" -eq 1 ]; then
+		echo "Random mode: ON"
+		shuf --output="$m3u_file_path" "$m3u_file_path"
+	fi
+
+	# Begin the new playlist file with the required file header (needed for UMS).
+	echo "#EXTM3U" | cat - "$m3u_file_path" > temp && mv temp "$m3u_file_path"
+
+	# Print out the playlist file for verification.
+	echo "Contents of m3u file:"
+	cat "$m3u_file_path"
+
+}
+
+# Get script arguments.
+get_arguments() {
+	while getopts ":dpr" option; do
+		case $option in
+			r)
+				randomized_m3u=1
+				;;
+			d)
+				download_playlist
+				exit
+				;;
+			p)
+				create_m3u
+				exit
+				;;
+			/?)
+				echo "Invalid option."
+				exit 1
+		esac
+	done
+}
+
+
+
+
+############
+### Main ###
+############
+main() {
+	get_arguments "$@"
+
+	download_playlist && echo "Downloading complete!"
+
+	create_m3u && echo "Playlist created!"
+
+	echo "All done."
+}
+
+# Call to main.
+main "$@"
