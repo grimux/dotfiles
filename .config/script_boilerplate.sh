32c0f5bd90ac3f034798bb11c106f57d48657d11
grimux <jakerm13@gmail.com>
2023-05-27 11:31:31 -0500

    Just added a header.

--- .config/script_boilerplate.sh
+++ .config/script_boilerplate.sh
@@ -16,6 +16,9 @@
 # The `x` flag is very useful when debugging.
 #---------------------------------------------------------------------------------------------------
 
+###################
+### Fail Events ###
+###################
 # e - script stops on error
 # u - error if undefined variable
 # o pipefail - script fails if command piped fails
