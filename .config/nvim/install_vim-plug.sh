5e408c8218ab6351e35b6518078a6d9b45fe8982
grimux <jakerm13@gmail.com>
2023-12-04 12:38:56 -0600

    Broke up code into more functions. Added update feature.

--- .config/nvim/install_vim-plug.sh
+++ .config/nvim/install_vim-plug.sh
@@ -19,6 +19,20 @@ vimplug_website="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug
 # Plugin file location
 vimplug_location="$HOME/.local/share/nvim/site/autoload/plug.vim"
 
+# Yes or no.
+function yes_or_no() {
+	local choice
+	local prompt="$1"
+
+	while true; do
+		read -p "$prompt (y/n): " choice
+		case $choice in
+			[Yy]*) return 0;;
+			[Nn]*) return 1;;
+		esac
+	done
+}
+
 # Check to see if vim-plug is already installed.
 function check_install() {
 	local install_location="$1"
@@ -32,7 +46,8 @@ function check_install() {
 }
 
 # Install vim-plug.
-function install_vimplug(){
+function install_vimplug() {
+	echo "Installing vim-plug..."
 	curl \
 		--fail \
 		--location \
@@ -41,20 +56,44 @@ function install_vimplug(){
 		"$vimplug_website"
 }
 
+# Install all vim-plug plugins.
+function install_plugins() {
+	nvim -c PlugInstall -c quitall
+}
+
+# Upgrade and update plugins.
+function update_plugins() {
+	echo "Checking for updates..."
+	nvim -c PlugUpgrade -c PlugUpdate -c quitall
+}
+
 # Main function.
 function main() {
 	local is_vimplug_installed
 
+	# Variable for if vim-plug is installed or not.
+	# 1 yes, 0 no.
 	is_vimplug_installed=$(check_install "$vimplug_location")
 
+	# Main logic.
+	# If vim-plug is installed.
 	if [ "$is_vimplug_installed" -eq 1 ]; then
+		# Console output.
 		echo "vim-plug is already installed."
+		# Show info on the file using ls.
 		echo "Info: $(ls -la $vimplug_location)"
-		echo "Aborting..."
-		exit
+		# Prompt for updates, if yes then run the update_plugins function.
+		yes_or_no "Would you like to check for updates?" && update_plugins
+
+	# If vim-plug is not installed.
 	else
-		echo "Installing vim-plug..."
+		# Install vim-plug.
+		install_vimplug
 	fi
+
+	# All finished.
+	echo "All done."
 }
 
+# Call to main
 main
