8290a2d1d1928e10ebc7f884875cadde644262dc
grimux <jakerm13@gmail.com>
2024-11-20 10:43:35 -0600

    A new file for setting configuration options for all my scripts. This script will serve as a configuration file for my scripts. This will allow for easy changing of settings in one file, instead of each script containing the settings. I'm not sure yet if I like this approach, but I want to try it out for a while.

---
+++ .config/script_config.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+#
+####################
+#
+# Name: Script Config
+# Author: Jefferey
+# Description: A settings file for all our various scripts.
+#
+####################
+
+
+############
+### TODO ###
+############
+#
+# - Add more comments.
+#
+############
+
+
+############
+### Eyes ###
+############
+# Name:
+# Location: .local/bin/peripherals/eyes
+# Use:
+HURT_CONF="hurt_high.conf"
+OKAY_CONF="normal_low.conf"
+
+
+############
+### Game ###
+############
+# Name:
+# Location: .local/bin/peripherals/eyes
+# Use:
+#
+SAVE_DIR_NAME="saves"
+OLD_SAVE_DIR_NAME="saves-old"
+REMOTE_DIR_NAME=""
+
+
+#################
+### Game Time ###
+#################
+# Name:
+# Location: .local/bin/peripherals/eyes
+# Use:
+#
+USE_MANGOHUD=1
+USE_MANGOHUD=1
+USE_BIGPICTURE=0
+CHOOSE_INTERACTIVELY=1		# Launch with menu by default.
+MENU_APPLICATION="DMENU"	# fzf,zenity,dmenu
+LAUNCH_STEAM=1
+LAUNCH_LUTRIS=0
+MINIMIZE_LUTRIS=0
+MINIMIZE_STEAM=0
+
+
+#################
+### Functions ###
+#################
+#
+
+
+#########################
+### Testing Functions ###
+#########################
+#
+
+
+
